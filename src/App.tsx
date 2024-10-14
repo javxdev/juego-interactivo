40d62fc95abb2178a9852cdb68429ac0bcad9b05
javxdev <javxdev@gmail.com>
2024-10-14 15:28:06 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <header className="max-w-3xl mx-auto pt-40 pb-10">
+        <p className='text-3xl text-gray-200 uppercase'>Juego interactivo - TypeScript</p>
+      </header>
+
+      <div className="max-w-3xl mx-auto py-10">
+        <p className='text-3xl text-gray-200 uppercase'>TEST</p>
+      </div>
+    </>
+  )
+}
+
+export default App
